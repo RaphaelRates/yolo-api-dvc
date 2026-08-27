5e8650bb6faf00c358277c77151d8602286b33f8
RaphaelRates <rafaelrats3@gmail.com>
2026-08-27 02:33:58 -0300

    fix(app): fix logic erros with exceptions

--- app/model.py
+++ app/model.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import torch
 from ultralytics import YOLO
 
-
 # Ajuste para PyTorch 2.6+:
 # desativa temporariamente o comportamento weights_only
 # durante o carregamento dos pesos do YOLO.
